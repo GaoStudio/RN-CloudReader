1e8d44c8c3ee4c7a7ccf2d31da672ac99d41da8e
Jaye Gao <810915983@qq.com>
2017-05-20 17:34:17 +0800

    完善每日推荐

--- src/page/Tech/DailyPage.js
+++ src/page/Tech/DailyPage.js
@@ -6,6 +6,8 @@ import {
     StyleSheet,
     Text,
     Image,
+    RefreshControl,
+    ScrollView,
     Button,
     View
 } from 'react-native';
@@ -42,8 +44,17 @@ export default class DailyPage extends Component{
         }
         this.refs.MyLoading.stopLoadAnimate();
         return(
-            <View>
-                <Swiper height={150} style={{flex:1,width:'100%'}} showsButtons={true}>
+            <ScrollView  refreshControl={
+                  <RefreshControl
+                    refreshing={this.state.isRefreshing}
+                    tintColor="#ff0000"
+                    title="Loading..."
+                    titleColor="#00ff00"
+                    colors={['#ff0000', '#00ff00', '#0000ff']}
+                    progressBackgroundColor="#ffffff"
+                  />
+                }>
+                <Swiper autoPlay={true} height={150} style={{flex:1,width:'100%'}} showsButtons={true}>
                     <Image
                         resizeMode={Image.resizeMode.cover}
                         source={require('../../../res/images/banner01.jpg')}style={{width:'100%',height:150}}></Image>
@@ -60,7 +71,26 @@ export default class DailyPage extends Component{
                         resizeMode={Image.resizeMode.cover}
                         source={require('../../../res/images/banner05.jpg')}style={{width:'100%',height:150}}></Image>
                 </Swiper>
-            </View>
+                <View style={{marginTop:15,marginBottom:10,flex:1,flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}>
+                    <View style={{alignItems:'center'}}>
+                        <Image source={require('../../../res/images/home_icon_fm.png')}style={{width:50,height:50}}></Image>
+                        <Text  style={{fontSize:12,marginTop:5}}>干货闲读</Text>
+                    </View>
+                    <View style={{alignItems:'center'}}>
+                        <Image source={require('../../../res/images/home_icon_day.png')} style={{alignItems:'center',justifyContent:'center',width:50,height:50}}>
+                            <Text style={{fontSize:25,color:'#D33A31'}}>
+                                20
+                            </Text>
+                        </Image>
+                        <Text style={{fontSize:12,marginTop:5}}>每日推荐</Text>
+                    </View>
+                    <View style={{alignItems:'center'}}>
+                        <Image source={require('../../../res/images/home_icon_rank.png')} style={{width:50,height:50}}></Image>
+                        <Text  style={{fontSize:12,marginTop:5}}>电影排行</Text>
+                    </View>
+                </View>
+                <View style={{width:'100%',height:1,backgroundColor:'#d4d4d4'}}></View>
+            </ScrollView >
         )
     }
 }
